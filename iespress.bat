d13390eafb4e7f38897dd215c14a376094a53e88
traybourne <traybourne@hotmail.com>
2019-03-18 16:25:07 -0700

    Update iespress.bat

--- iespress.bat
+++ iespress.bat
@@ -6,7 +6,6 @@ for /F "delims=" %%A in ("%ARG0%") do set WDIR=%%~dpA
 set WDIR=%WDIR:~0,-1%
 
 for /F "delims=" %%A in ('cscript "%WDIR%\iespress.vbs" 3') do set WDIR=%%~dpA
-if ERRORLEVEL 1 goto ERROR
 set WDIR=%WDIR:~0,-1%
 
 echo .\excludes.txt>excludes.txt
@@ -17,8 +16,10 @@ del /F /Q excludes.txt
 
 cd "%WDIR%"
 start %*
+EXIT
 
 :ERROR
+cls
 echo ERROR OCCURRED - CHECK THE PATH ENVIRONMENT VARIABLE
 PAUSE
 EXIT
